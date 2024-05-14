2fd244b9b8925c3323752ca0ac5b5112cc98e26b
Matheus Alves <matheus.jeje2@gmail.com>
2024-05-14 14:26:29 -0300

    Adicionando e configurando personagem

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 import pygame
+from personagem import Jogador
 
 pygame.init()
 #configuração da tela
@@ -7,8 +8,10 @@ tela = pygame.display.set_mode((900, 700))
 pygame.display.set_caption("Comilão")
 
 
-FUNDO = pygame.image.load("Fundo_do_mar.png")
+FUNDO = pygame.image.load("Imagens/Fundo_do_mar.png")
 FUNDO = pygame.transform.scale(FUNDO,(900, 700))
+
+jogador1 = Jogador("Imagens/peixe.png",50,50,0,650 )
 
 clock = pygame.time.Clock()
 
@@ -18,7 +21,11 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+            
     tela.blit(FUNDO, (0,0))
+
+    jogador1.movimenta_via_controle(pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
+    jogador1.apareca(tela)
 
     pygame.display.update()
 
