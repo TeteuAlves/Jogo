5bba173eb29462487e5dee5bb8140c16dd00cb91
Matheus Alves <matheus.jeje2@gmail.com>
2024-05-14 16:41:49 -0300

    Pontuação

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
 import pygame
 from personagem import Jogador
-from obstaculos import Obstaculo
+from obstaculos_bons import Obstaculo
+from obstaculos_ruins import Buff
 
 pygame.init()
 #configuração da tela
@@ -15,9 +16,21 @@ FUNDO = pygame.transform.scale(FUNDO,(900, 700))
 #Adicionando jogador
 jogador1 = Jogador("Imagens/peixe.png",100,100,400,600 )
 
-lista_objeto = [Obstaculo("imagens/anzol.png",75,75,500,10),
-                Obstaculo("imagens/salsicha.png",75,75,500,10)]
+lista_objeto_bonus = [Obstaculo("imagens/salsicha.png",75,75,0),
+                    Obstaculo("imagens/Ração.png",75,75,0),
+                    Obstaculo("imagens/salsicha.png",75,75,0),
+                    Obstaculo("imagens/Ração.png",75,75,0)]
 
+lista_objeto_ruins = [Buff("imagens/anzol.png",75,75,0),
+                        Buff("imagens/anzol.png",75,75,0),
+                        Buff("imagens/anzol.png",75,75,0)]
+
+fonte = pygame.font.SysFont("Arial", 50)
+
+#pontuação
+pontuação_personagem = 0
+
+pontuação_vida = 3
 #configurar FPS
 clock = pygame.time.Clock()
 
@@ -30,14 +43,38 @@ while rodando:
 
     tela.blit(FUNDO, (0,0))
 
+#movimentando e aparecendo jogador
     jogador1.movimenta_via_controle(pygame.K_LEFT, pygame.K_RIGHT)
     jogador1.apareca(tela)
 
-    for obstaculos in lista_objeto:
+#movimentando objeto
+    for obstaculos in lista_objeto_bonus:
         obstaculos.movimenta()
         obstaculos.apareca(tela)
+        if jogador1.mascara.overlap(obstaculos.mascara,(obstaculos.pos_x - jogador1.pos_x, obstaculos.pos_y - jogador1.pos_y )):
+            pontuação_personagem = pontuação_personagem + 1
+            obstaculos.pos_y = 700
+            break
 
+    for ruim in lista_objeto_ruins:
+        ruim.movimenta()
+        ruim.apareca(tela)
+        if jogador1.mascara.overlap(ruim.mascara,(ruim.pos_x - jogador1.pos_x, ruim.pos_y - jogador1.pos_y )):
+            pontuação_vida = pontuação_vida - 1
+            ruim.pos_y = 700
+            if pontuação_vida == 0:
+                rodando = False
+#contagem
+    texto_pontos = fonte.render(f"Pontuação:{pontuação_personagem} ", False,(255,0,0))
+
+    texto_vidas = fonte.render(f"Vidas:{pontuação_vida} ", False,(255,0,0))
+
+    tela.blit(texto_pontos,(600,0))
+    tela.blit(texto_vidas,(100,0))
+#atualizar a tela
     pygame.display.update()
 
     #REGULAR O FPS
     clock.tick(60)
+
+print("Você perdeu!!!")
---
+++ obstaculos_bons.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+class Obstaculo:
+    def __init__(self, arquivo_imagem, largura_imagem, altura_imagem, y_inicial ):
+        self.imagem = pygame.image.load(arquivo_imagem)
+
+        self.largura = largura_imagem
+        self.altura = altura_imagem
+
+        self.imagem = pygame.transform.scale(self.imagem, (self.largura, self.altura))
+
+        self.pos_x = random.randint(10,850)
+        self.pos_y = y_inicial
+
+        self.velocidade = random.randint(1,8)
+
+        self.mascara = pygame.mask.from_surface(self.imagem)
+
+    def apareca(self, tela):
+        tela.blit(self.imagem, (self.pos_x, self.pos_y))
+
+    def movimenta(self):
+        self.pos_y = self.pos_y + self.velocidade
+        if self.pos_y >= 650:
+            self.pos_y = 10
+            self.velocidade = random.randint(1,8)
+            self.pos_x = random.randint(10,850)
