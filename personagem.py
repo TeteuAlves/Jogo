c8a5538789db5359512257e12b55f4f68484fd3a
Matheus Alves <matheus.jeje2@gmail.com>
2024-05-14 17:10:00 -0300

    commit final

--- personagem.py
+++ personagem.py
@@ -1,6 +1,7 @@
 import pygame
-import random
-class Obstaculo:
+
+class Jogador:
+
     def __init__(self, arquivo_imagem, largura_imagem, altura_imagem, x_inicial, y_inicial ):
         self.imagem = pygame.image.load(arquivo_imagem)
 
@@ -12,15 +13,22 @@ class Obstaculo:
         self.pos_x = x_inicial
         self.pos_y = y_inicial
 
-        self.velocidade = random.randint(10,15)
-
         self.mascara = pygame.mask.from_surface(self.imagem)
 
     def apareca(self, tela):
         tela.blit(self.imagem, (self.pos_x, self.pos_y))
 
-    def movimenta(self):
-        self.pos_y = self.pos_y + self.velocidade
-        if self.pos_y >= 600:
-            self.pos_y = 10
-            self.velocidade = random.randint(10,15)
+    def movimenta_via_controle(self,esquerda,direita,poder):
+        teclas = pygame.key.get_pressed()
+        if teclas[direita]:
+            if self.pos_x < 800:
+               self.pos_x = self.pos_x + 20
+
+        if teclas[esquerda]:
+            if self.pos_x > 0:
+              self.pos_x = self.pos_x - 20
+
+        if teclas[poder]:
+            self.pos_x = 0
+            self.pos_y = 600
+            
